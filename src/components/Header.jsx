ce3d5f0dc2a52f887f06b31e70453b46a609f9c2
abhaychauhan8802 <abhaychauhan8802@gmail.com>
2024-06-12 10:46:53 +0530

    fix: change favicon and add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
+import logo from "/logo.png";
 
 const Header = () => {
   return (
     <div className="bg-background border-input border-b-2 px-4 md:px-8 h-16 flex items-center justify-between">
-      <h1 className="text-2xl font-bold select-none">CodeWriter</h1>
+      <div className="flex gap-1 items-center select-none">
+        <img src={logo} alt="logo" className="w-8 invert mt-1" />
+        <h1 className="text-2xl font-bold">CodeWriter</h1>
+      </div>
       <Link to="/compiler">
         <Button variant="link">Compiler</Button>
       </Link>
